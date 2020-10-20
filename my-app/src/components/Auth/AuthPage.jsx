af654c2f5713e0822459daab342a821474b147a8
HDOG <Hossamsulleman@gmail.com>
2020-10-20 10:53:44 +0100

    kj

---
+++ my-app/src/components/Auth/AuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AuthPage() {
+    return(
+<div>
+    <p>
+hi
+    </p>
+</div>
+    );
+}
---
+++ my-app/src/components/Auth/GoogleA.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function GoogleA() {
+
+return (
+<div>
+    <p>hi</p>
+</div>
+
+);
+}
+
+export default GoogleA;
